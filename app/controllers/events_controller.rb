18a68a21e69824cdc571ca190d0f3d582ecda089
James Okunlade <james.okunlade@gmail.com>
2019-04-26 12:25:30 +0100

    Event controller created with necessary methods

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,44 @@
+class EventsController < ApplicationController
+
+  include SessionsHelper
+
+    before_action :logged_in, only: [:new, :create]
+
+
+    def index
+        @events = Event.all
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.events.build(event_params)
+        if @event.save
+            flash[:success] = "Event created!"
+            redirect_to root_url
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @event = Event.find_by(id: params[:id])
+        # @event = Event.find(params[:id])
+        # @creator = @event.creator
+    end
+
+    private
+
+        def event_params
+            params.require(:event).permit(:name, :description, :dateofevent, :location)
+        end
+
+        def logged_in
+            unless logged_in?
+              flash[:danger] = "Please login first!!!"
+              redirect_to login_path
+            end
+        end
+end
