1da5f0597f1b1fba6507032d7aec455df85b46d5
Adnan Afsari <adnanafsari.swe@gmail.com>
2021-11-21 16:25:31 +0600

    fix::change mirgration

--- db/migrate/20190426113813_add_creator_to_event.rb
+++ db/migrate/20190426113813_add_creator_to_event.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddCreatorToEvent < ActiveRecord::Migration[5.2]
   def change
-    add_reference :events, :creator, foreign_key: true
+    add_reference :events, :user, foreign_key: true
   end
 end
